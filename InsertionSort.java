b9ff676dade650b517d8cbb5cd51c79e537612c5
raxitcholera <raxitcholera@gmail.com>
2017-01-26 09:57:20 -0500

    Correction
    The last element was not getting run, corrected it with this commit.

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,13 +2,14 @@ public class InsertionSort {
 
 	public static void main(String[] args){
         int[] num = {5, 8, 24, 15, 200, 192, 86, 35, 78, 4 ,9, 20};
+        // int[] num = {5,2,8,3,4,1};
         printAll(num);
         insertionSort(num);
         printAll(num);
 	}
 	public static void insertionSort (int[] num){
 		int i,j,temp,index;
-		for(i=0;i<num.length-1;i++) {
+		for(i=0;i<num.length;i++) {
 			temp=num[i];
 			j=i-1;
 			while(j>=0 && num[j]>temp)
