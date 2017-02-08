55e7fa8a1f9fc8d6f01cfb98f5c780dc3de0eb2e
raxitcholera <raxitcholera@gmail.com>
2017-02-08 10:28:40 -0500

    Added Recursive call

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,7 +3,8 @@ public class BubbleSort {
 	public static void main(String[] args){
         int[] num = {5, 8, 24, 15, 200, 192, 86, 35, 78, 4 ,9, 20};
         printAll(num);
-        bubbleSort(num);
+        recursiveBubbleSort(num,12,0);
+        //bubbleSort(num);
         printAll(num);
 	}
 	public static void bubbleSort (int[] num){
@@ -18,6 +19,21 @@ public class BubbleSort {
 			}
 		}
 	}
+	public static int[] recursiveBubbleSort(int[] list, int n,int start) {
+        if (start == n) {
+            return list; //finished sorting
+        }
+        
+        int temp;
+        for (int i = n-1; i > start; i--) {
+            if (list[i] < list[i-1]) {
+                temp = list[i];
+                list[i] = list[i-1];
+                list[i-1] = temp;
+            }
+        }
+        return recursiveBubbleSort(list, n, start+1);
+    }
 	public static void printAll(int[] num) {
 		for(int i = 0; i < num.length; i++){
             System.out.print("\t" + num[i]);
